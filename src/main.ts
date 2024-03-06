c5c24bfae931ac463634c9dc12edfa201da55428
gri-gri <46087070+gri-gri@users.noreply.github.com>
2024-03-06 13:17:38 +0300

    feat: add nest.js project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<number>('PORT', 3000);
+
+  await app.listen(port);
+}
+bootstrap();
